07626c0a60f7b77e2186b8afd4e7559a90e379e5
Ryan Lee <therealsassyfatcat@outlook.com>
2020-07-07 15:05:15 -0500

    working

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-// Your code goes here
+//MOUSEOVER -------------------------------------->
+const parag = document.querySelectorAll('p');
+parag.forEach(x => {
+    x.addEventListener('mouseover', event => event.target.style.backgroundColor = 'lightgrey');
+    x.addEventListener('mouseout', event => event.target.style.backgroundColor = '');
+});
+
+//COPY ---------------------------------------->
+document.querySelectorAll('*').forEach(x => {
+    x.addEventListener('copy', (event) => {
+        alert("Your selection has been copied to your clipboard");
+        event.stopPropagation();});
+});
+
+// HOVER/ZOOM ON IMAGES
+const images = document.querySelectorAll('img');
+images.forEach(x => {
+    x.addEventListener('mouseover', (event) => {
+        setTimeout(() => {
+            event.target.style.transform = 'scale(1.5)';
+            event.target.style.transition = 'transform .8s';
+        }, 1000)
+    });
+    x.addEventListener('mouseout', (event) => {
+        event.target.style.transform = 'scale(1)';
+    })
+})
